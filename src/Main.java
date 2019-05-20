15638b9c234150859ad71f2259c9ecc018b8ae3c
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-05-20 12:54:08 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
         Escape.displayEscape();
         thirdfloor.thirdfloorA();
 
-
     }
 }
--- src/thirdfloor.java
+++ src/thirdfloor.java
@@ -1,11 +1,7 @@
 import javax.swing.plaf.synth.SynthLookAndFeel;
 
 public class thirdfloor {
-    public static void thirdfloorA(int bat){
-        if (bat==0)
-            System.out.println("Bat works");
-        else
-            System.out.println("Bat don't work");
+    public static void thirdfloorA(){
 
     }
 
