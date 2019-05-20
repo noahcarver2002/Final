c42748cb9ca46c1bd069444a2c43d0741b65e7fc
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-05-20 12:38:41 -0500

    update this

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args){
-        int bat=0;
-        Escape.displayEscape(bat);
+        Escape.displayEscape();
+        thirdfloor.thirdfloorA(b);
 
 
     }
